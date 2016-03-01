dee99bc3c141fc76f029c35c0a768a20be9e82ae
Daniel Petti <djpetti@gmail.com>
2016-03-01 11:50:19 -0500

    Add script for running theanets container.

---
+++ theanets/run_theanets.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo docker run -ti --device /dev/nvidia0:/dev/nvidia0 --device \
+  /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
+  --net=host theanets /bin/bash
