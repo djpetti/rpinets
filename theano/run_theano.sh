57237157ac04fd9b30df0ab9f1877235da5174fe
Daniel Petti <djpetti@gmail.com>
2016-05-11 11:01:15 -0400

    Use new docker containers for theano.
    This is a much better system than the messy one we had before.
    There is now a core container for theano, and it mounts all the
    code as a volume.

--- theano/run_theano.sh
+++ theano/run_theano.sh
@@ -2,4 +2,4 @@
 
 sudo docker run -ti --device /dev/nvidia0:/dev/nvidia0 --device \
   /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
-  --net=host theanets /bin/bash
+  -v "$(dirname "`pwd`")":/home/theano/research --net=host djpetti/theano /bin/bash
