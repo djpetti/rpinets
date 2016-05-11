af7615cb9c4fd97e17bce4bc561ddd916a201c28
Daniel Petti <djpetti@gmail.com>
2016-05-11 11:55:05 -0400

    Add specialized container for rpinets.
    This includes eveything we need to run specifically rpinets code.

--- theano/run_theano.sh
+++ theano/run_theano.sh
@@ -2,4 +2,4 @@
 
 sudo docker run -ti --device /dev/nvidia0:/dev/nvidia0 --device \
   /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm \
-  -v "$(dirname "`pwd`")":/home/theano/research --net=host djpetti/theano /bin/bash
+  -v "$(dirname "`pwd`")":/home/theano/research --net=host djpetti/rpinets-theano /bin/bash
