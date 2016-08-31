bc7bc2c56b733f3e19bb05445d90895c9cb4a420
Daniel Petti <djpetti@gmail.com>
2016-08-31 16:04:46 -0400

    Make run_theano create a link to training data.
    Previously, this was stored within the rpinets repository itself,
    so it wasn't a big issue. However, now I want that flexibility.

--- theano/run_theano.sh
+++ theano/run_theano.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo nvidia-docker run -ti -v "$(dirname "`pwd`")":/home/theano/research --net=host djpetti/rpinets-theano /bin/bash
+# Check the number of arguments.
+if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
+  echo "Usage: $0 TRAINING_DATA_DIR" >&2
+  exit 1
+fi
+
+sudo nvidia-docker run -ti -v "$(dirname "`pwd`")":/home/theano/rpinets \
+    -v "$1":/home/theano/training_data --net=host djpetti/rpinets-theano /bin/bash
